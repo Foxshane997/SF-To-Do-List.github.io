855c6331d4d7fc814214b3466f13c5f99f837b8f
“Foxshane997” <Foxshane997@gmail.com>
2024-06-10 18:02:36 -0700

    Almost finished just need to add delete functionality

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addToDo = (todo) => {
+    const newToDo = {
+      id: Math.random(),
+      todo: todo
+    }
+
+    // Add to the list
+    setList([...list, newToDo])
+
+    // Clear the input 
+    setInput("")
+  }
+
+  return (
+    <div className="App">
+      <h1>To Do List</h1>
+      <input 
+        type='text' 
+        value={input} 
+        onChange={(e) => setInput(e.target.value)} 
+      />
+      <button onClick={() => addToDo(input)} >Add To Do</button>
+      <ul>
+        {list.map((todo) => (
+          <li key={todo.id}>
+            {todo.todo}
+            <button>&times;</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
